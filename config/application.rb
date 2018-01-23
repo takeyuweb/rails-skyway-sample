0379fb5950d13eb38063b2d78e61c2e14a0d9df6
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2018-01-24 01:34:35 +0900

    Add webpacker

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module SkyMeeting
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.generators do |g|
+      g.assets false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
